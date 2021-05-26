95ae56b2450d233d2f2c346b9fce48d6f56afed8
juddmorse <59372868+juddmorse@users.noreply.github.com>
2021-05-25 21:14:08 -0700

    practice platformer in unity. nothing important
    practice platformer in unity. nothing important

---
+++ test2/Poker_test/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+    private int superJumpsRemaining;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if space key is pressed down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // FixedUpdate is called once every physics update
+    private void FixedUpdate()
+    {
+        rigidBodyComponent.velocity = new Vector3(horizontalInput, rigidBodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length ==0)
+        {
+            return;
+        }
+        
+        if(jumpKeyWasPressed)
+        {
+            float jumpPower = 7f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidBodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+    }
+}
